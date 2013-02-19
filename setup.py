c9f710ea6c3ba9b1434f7cf2029432797901946f
Mihai Mocanu <mihai.form@gmail.com>
2013-02-19 15:35:29 +0200

    Renamed Readme and added the content of it to setup.py long_description parameter

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(name='django-clicktale',
       version='1.0.0',
       description=('A simple Django application to integrate ClickTale into '
                    'your projects'),
+      long_description=open('README').read(),
       author='TPG PBS Core Services',
       keywords="django clicktale integration",
       author_email='TPG-PBS-CoreServices@3pillarglobal.com',
